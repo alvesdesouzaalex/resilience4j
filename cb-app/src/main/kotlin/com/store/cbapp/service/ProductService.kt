e51d22c0e1aeedba38cce948cb12d9e8334bdcaf
aasouza <alvesdesouzaalex@gmail.com>
2023-09-22 12:39:46 -0300

    ajuste

--- cb-app/src/main/kotlin/com/store/cbapp/service/ProductService.kt
+++ cb-app/src/main/kotlin/com/store/cbapp/service/ProductService.kt
@@ -2,8 +2,9 @@ package com.store.cbapp.service
 
 import com.store.cbapp.client.ProductClient
 import com.store.cbapp.exception.ServiceUnavailableException
+import com.store.cbapp.model.Brand
 import com.store.cbapp.model.Product
-import feign.FeignException
+import feign.FeignException.ServiceUnavailable
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import mu.KLogging
@@ -21,9 +22,15 @@ class ProductService(
     fun getProducts() = productClient.getProducts()
 
 
+    fun getProductFallback(ex: ServiceUnavailable): List<Product> {
+        logger.info("ServiceUnavailableException - Service took to long to respond")
+        throw ServiceUnavailableException("Service took to long to respond")
+    }
+
     fun getProductFallback(ex: CallNotPermittedException): List<Product> {
-        logger.info ("Service took to long to respond")
-        throw ServiceUnavailableException(ex.message)
+        logger.info("CallNotPermittedException - Service took to long to respond")
+        val product = Product("123", "Product Fallback", Brand("Olympicus", "running"), 0.0, 0)
+        return listOf(product)
     }
 
 }
